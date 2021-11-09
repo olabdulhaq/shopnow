c7899427f22a41a403d8753b2442a52b6be50363
addegbenga <addegbenga@gmail.com>
2021-11-09 02:54:03 -0500

    changes

---
+++ src/components/shop/shopHome.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function shopHome() {
+    return (
+        <div>
+            Shop homepage
+        </div>
+    )
+}
---
+++ src/components/shop/shopProduct.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function shopProduct() {
+    return (
+        <div>
+            shop product
+        </div>
+    )
+}
